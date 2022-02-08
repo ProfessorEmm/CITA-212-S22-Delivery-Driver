37b588e9709033413d6497b4b3bf5bd57c211036
Richard Marcoux <marcourp@morrisville.edu>
2022-02-08 13:02:51 -0500

    Using OnCollisionEnter2D()

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("I've collided with something!");    
+    } 
+} // class Collision
