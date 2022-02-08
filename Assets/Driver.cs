96af2c2073d3753580e2e3060d1f85f1aca4a93b
Richard Marcoux <marcourp@morrisville.edu>
2022-02-07 20:25:01 -0500

    Introducing Variables

---
+++ Assets/Driver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    float fltSteerSpeed = 1f;
+    [SerializeField] float fltMoveSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, fltSteerSpeed);
+        transform.Translate(0, fltMoveSpeed, 0);
+    }
+}
